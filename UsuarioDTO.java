fe97575f3f0b6cff8a173aa379967d332420394c
Apocalypsk <kathleenribeiro350@gmail.com>
2025-06-09 23:10:21 -0300

    UsuarioDTO
    classe responsável por armazenar, carregar e transportar os dados. 
    Contém os métos getters e setters.

---
+++ UsuarioDTO.java
@@ -0,0 +1,51 @@
+package DTO;
+
+
+public class UsuarioDTO {
+    private String telefone, nome, email;
+
+    /**
+     * @return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    
+}
